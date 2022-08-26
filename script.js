0a7d1a9aad104bb911ec4829c4baab714716c488
unknown <sizwenqwala@gmail.com>
2022-08-26 15:39:18 +0200

    added html and  js

--- script.js
+++ script.js
@@ -1,114 +1,138 @@
-// PRODUCTS VARIABLE
-var productsState = [
+
+// VARIABLES
+var StoreData = [
   {
-    id: 1,
-    name: 'Macbook Pro',
-    price: 28000,
-    rates: 5,
-    image:
-      'https://www.istore.co.za/media/catalog/product/m/a/macbook_pro_space_grey-1_2.png?format=jpeg',
-  },
-  {
-    id: 2,
-    name: 'Iphone 13 Pro',
-    price: 23000,
-    rates: 3,
-    image:
-      'https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
-  },
-  {
-    id: 1,
-    name: 'Iphone 6 C',
-    price: 7000,
-    rates: 2,
-    image:
-      'https://images.unsplash.com/photo-1565849904461-04a58ad377e0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80',
-  },
-  {
-    id: 1,
-    name: 'HP Laptop',
-    price: 8000,
-    rates: 1,
-    image:
-      'https://images.unsplash.com/photo-1589561084283-930aa7b1ce50?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1631&q=80',
-  },
-  {
-    id: 1,
-    name: 'Dell Laptop',
-    price: 4000,
-    rates: 1,
-    image:
-      'https://images.unsplash.com/photo-1588872657578-7efd1f1555ed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-  },
-  {
-    id: 1,
-    name: 'Macbook Pro',
-    price: 15000,
-    rates: 5,
-    image:
-      'https://images.unsplash.com/photo-1611186871348-b1ce696e52c9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-  },
-  {
-    id: 1,
-    name: 'Playstation 5',
-    price: 20000,
-    rates: 4,
-    image:
-      'https://images.unsplash.com/photo-1606144042614-b2417e99c4e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-  },
-  {
-    id: 1,
-    name: 'PS 4 Joystick',
-    price: 1200,
-    rates: 3,
-    image:
-      'https://images.unsplash.com/photo-1592840496694-26d035b52b48?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=825&q=80',
-  },
+    Number: 1,
+    category: 'MAC',
+    costs: 4,
+    money: 12000,
+    image: 'https://www.istore.co.za/media/catalog/product/m/g/mgnd3zea.jpeg?optimize=medium&bg-color=255,255,255&fit=bounds&height=700&width=700&canvas=700:700',
+
+},
+
+{
+  Number: 2,
+  category: 'iPhone 13 mini',
+  costs: 3,
+  money: 16000,
+  image: 'https://www.istore.co.za/media/catalog/product/i/p/iphone_13_green_pdp_image_position-1a__wwen_4_1.png?format=jpeg',
+
+},
+
+{
+  Number: 3,
+  category: 'iPad Pro M1',
+  costs: 4,
+  money: 16999,
+  image: 'https://www.istore.co.za/media/catalog/product/i/p/ipad_pro_wi-fi_12.9_in_space_gray_pdp_image_position-1b__wwen_1.jpg?optimize=medium&bg-color=255,255,255&fit=bounds&height=700&width=700&canvas=700:700',
+
+},
+
+{
+  no: 4,
+  category: 'Watch Series 7',
+  costs: 5,
+  money: 6290,
+  image: 'https://www.istore.co.za/media/catalog/product/s/t/starlight_2.jpg?optimize=medium&bg-color=255,255,255&fit=bounds&height=300&width=300&canvas=300:300',
+
+},
+
+{
+  Number: 5,
+  category: 'AirTag',
+  costs: 3,
+  money: 700,
+  image: 'https://www.istore.co.za/media/catalog/product/a/i/airtag_white_pure_front_screen__usen_1_1.jpg?optimize=medium&bg-color=255,255,255&fit=bounds&height=300&width=300&canvas=300:300',
+
+},
+
+{
+  Number: 6,
+  category: 'New Balance Fire',
+  costs: 4,
+  money: 1100,
+  image: 'https://th.bing.com/th/id/OIP.DSuzP9FSUAEKDwt84zECEQHaHa?w=177&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7',
+
+},
+
+{
+  Number: 7,
+  category: 'Apple TV HD 6th Gen - 32GB',
+  costs: 5,
+  money: 5199,
+  image: 'https://www.istore.co.za/media/catalog/product/a/p/apple_tv_4k_siri_remote_pure_front_screen__usen_2.jpg?optimize=medium&bg-color=255,255,255&fit=bounds&height=300&width=300&canvas=300:300',
+
+},
+
+{
+  Number: 8,
+  category: 'Apple AirPods 2 With Charging Case',
+  costs: 4,
+  money: 2567,
+  image: '',
+
+},
 ]
 
-var number = 0;
+// this is dislay to HTML in JS
+let ProductsiPhoneStore = document.getElementById('ProductsiPhoneStore');
+let displayAmount = document.getElementById('displayAmount');
+let cartTotal = document.getElementById('cartItems')
 
 
-// LINK JS TO HTML ELEMENT
-const products = document.getElementById('products')
-const product__name = document.getElementById('cart_number')
+// this is variables to JS
+let cartMath = 0;
+let productMoney = 0;
+let productcategory;
+let productcosts;
+let productImage;
 
 
-// DISPLAY PRODUCTS IN HOME PAGE
-function homeDisplayProducts() {
-    products.innerHTML = ""
-  // loop into productsState and display
-  for (let i = 0; i < productsState.length; i++) {
-    products.innerHTML += `
-    
-    <div class="product">
+
+
+function myCart() {
+  
+  cartNum = document.getElementById('cartNum').innerHTML = ++ cartMath;
+
+  displayCart.push({Number: displayCart.length +1,
+                      category: productcategory,
+                      costs: productcosts,
+                      money: productMoney,
+                      image: '',})
+
+  myDisplay();
+
+//   console.log('displayCart')
+}
+
+
+//i want to show myDisplay to HTML
+function myDisplay() {
+
+    ProductsiPhoneStore.innerHTML = ""
+
+    // this is show the number
+    for (let d = 0; d < StoreData.length; d++) {
+        ProductsiPhoneStore.innerHTML += 
+        
+        `<div class="product">
         <div class="product__img">
-            <img
-              src=${productsState[i].image}
-              alt=""
-            />
+          <img
+            src=${StoreData[d].image}
+            alt=""
+          />
         </div>
-            <div class="product__name">${productsState[i].name}</div>
-              <div class="product__rate">
-                ${'<span>*</span>'.repeat(productsState[i].rates)}
-              </div>
-              <div class="product__price">R <span>${productsState[i].price}</span></div> 
-                <button onclick="myCarts()">+ ADD TO CART</button> 
-          </div>
-    
-    `
-  }
+
+        <div class="product__category">${StoreData[d].category}</div>
+        <div class="product__cost">
+        ${'<span>*</span>'.repeat(StoreData[d].costs)}
+        </div>
+        <div class="product__price">R <span>${StoreData[d].money}</span></div>
+        <button onclick="myCart()">+ ADD TO CART</button>
+      </div>`
+        
+    }
+
+
 }
-
-
-// CALL THE DISPLAY FUNCTION
-homeDisplayProducts()
-
-// DISPLAY cart_number to HTML Element
-function myCarts() {
-      
-  product__name.innerHTML = ""
-
-  // loop to HTML cart
-}
-
+myDisplay();
